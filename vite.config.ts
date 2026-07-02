17e8ef008c74fe1efb49bcc60e893f49b514010e
Pierre Bossetti <pierrebossetti_348@hotmail.fr>
2026-07-02 19:08:22 +0200

    Setup du proxy + affichage token.

--- vite.config.ts
+++ vite.config.ts
@@ -8,4 +8,18 @@ export default defineConfig({
     react(),
     babel({ presets: [reactCompilerPreset()] })
   ],
+  server: {
+    proxy: {
+      '/api': { // Toutes les requêtes qui commencent par "/api"
+        target: 'http://localhost:8080', // sont redirigé sur le backend java
+        changeOrigin: true, // corrige les problèmes de CORS
+        rewrite: (path) => path.replace(/^\/api/, ''), // enlève le "/api", car le backend java ne connaît pas le chemin
+      },
+      '/ollama': { // Toutes les requêtes qui commencent par "/ollama"
+        target: 'http://localhost:11434', // sont redirigée sur le backend ollama
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/ollama/, ''),
+      }
+    }
+  },
 })
